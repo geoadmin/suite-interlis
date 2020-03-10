0fb8e0f51c05d1732c493e9e6c55c360a11dbad6
p1d1d1 <pasquale.didonato@gmail.com>
2020-03-10 11:32:22 +0100

    Add RKo.T11

--- routine/src/test/java/ch/interlis/testsuite/ConstraintTest.java
+++ routine/src/test/java/ch/interlis/testsuite/ConstraintTest.java
@@ -549,5 +549,45 @@ public class ConstraintTest {
 		logger.info(vendor + " - " + testName.getMethodName() +": " + ret);
 		assertFalse(ret);
 	}
+	
+	/**
+	 * @ID RKo.T11a
+	 *
+	 * @Designation Constraints: CONSTRAINTS OF
+ 	 *
+ 	 * @Description The tests must check whether the data satisfies the ConstraintDef in the INTERLIS data model
+ 	 *
+ 	 * @Test-requirement RKo.T11a.xtf
+ 	 *
+ 	 * @Expected-result No error message
+ 	 *
+ 	 * @Reference <p><a href="https://www.interlis.ch/download/interlis2/ili2-refman_2006-04-13_e.pdf">INTERLIS Version 2 – Reference Manual</a> Chap. 2.12, 2.13 and 2.14</p>
+ 	 */
+	@Test
+	public void RKo_T11a() {
+		boolean ret = TestUtil.runJob(vendor, "../data/RKo.T11a.xtf");
+		logger.info(vendor + " - " + testName.getMethodName() +": " + ret);
+		assertTrue(ret);
+	}
+	
+	/**
+	 * @ID RKo.T11b
+	 *
+	 * @Designation Constraints: CONSTRAINTS OF
+ 	 *
+ 	 * @Description The tests must check whether the data satisfies the ConstraintDef in the INTERLIS data model
+ 	 *
+ 	 * @Test-requirement RKo.T11b.xtf
+ 	 *
+ 	 * @Expected-result Error message. Constraint violated
+ 	 *
+ 	 * @Reference <p><a href="https://www.interlis.ch/download/interlis2/ili2-refman_2006-04-13_e.pdf">INTERLIS Version 2 – Reference Manual</a> Chap. 2.12, 2.13 and 2.14</p>
+ 	 */
+	@Test
+	public void RKo_T11b() {
+		boolean ret = TestUtil.runJob(vendor, "../data/RKo.T11b.xtf");
+		logger.info(vendor + " - " + testName.getMethodName() +": " + ret);
+		assertFalse(ret);
+	}
 
 }
